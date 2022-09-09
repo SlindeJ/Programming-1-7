3ab3c816f3236d526a73362728623faee03d76c0
SlindeJ <112727654+SlindeJ@users.noreply.github.com>
2022-09-09 23:44:42 +0000

    update prog54b.py

--- prog54b.py
+++ prog54b.py
@@ -6,5 +6,5 @@ d = float(input("Please enter d:"))
 sum = a + b + c + d
 average = sum / 4
 
-print("The sum of the four numbers is", sum)
+print("\nThe sum of the four numbers is", sum)
 print("\nThe average of the four nunbers is", average)
