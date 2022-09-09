5503a100b3340abb9c3448b86827be0dcd658c78
SlindeJ <112727654+SlindeJ@users.noreply.github.com>
2022-09-09 19:36:46 +0000

    prog52a.py

---
+++ prog54b.py
@@ -0,0 +1,10 @@
+a = float(input("Please enter a:"))
+b = float(input("Please enter b:"))
+c = float(input("Please enter c:"))
+d = float(input("Please enter d:"))
+
+sum = a + b + c + d
+average = sum / 4
+
+print("The sum of the four numbers is", sum)
+print("\nThe average of the four nunbers is", average)
